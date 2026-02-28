8fe51133f42f8cccf2060bd3692ab92ca7afecde
Lynn Haydar <lhaydar@c1r9s8.42beirut.com>
2026-02-28 19:46:06 +0200

    ex5 iterative done

---
+++ ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,7 @@
+def ft_count_harvest_iterative():
+    count = int(input("Days until harvest: "))
+    i = 1
+    while i <= count:
+        print("Day", i)
+        i += 1
+    print("Harvest time!")
