4ca89461dc854f73ba2dba40ce3e3e0abd55e020
Lynn Haydar <lhaydar@c1r9s8.42beirut.com>
2026-02-28 20:01:15 +0200

    ex5 recursive tested

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,1 +1,13 @@
 def ft_count_harvest_recursive():
+    count = int(input("Days until harvest: "))
+    i = 0
+    ft_recursion(count, i)
+
+
+def ft_recursion(count, i):
+    if count == i:
+        print("Harvest time!")
+    else:
+        i += 1
+        print("Day", i)
+        ft_recursion(count, i)
